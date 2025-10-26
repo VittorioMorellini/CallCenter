d852e159cb85d780c459a49842bf4e9ebd883c84
Vittorio Morellini <sxi0147@sixtema.it>
2025-10-26 17:39:34 +0100

    Adding Aspire configuring for port 3000

--- Callcenter.AppHost/Program.cs
+++ Callcenter.AppHost/Program.cs
@@ -2,19 +2,26 @@ using k8s.Models;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var apiService = builder.AddProject<Projects.Cc_Api>("cc-api");
+var apiService = builder.AddProject<Projects.Cc_Api>("cc-api")
+    .WithHttpHealthCheck("/health");
 
 builder.AddYarnApp("WebUi", "../Cc.Ui", "start")
-    .WithReference(apiService) // Reference the API service if the frontend needs to call it
-    .WithExternalHttpEndpoints() // Makes the frontend URL externally available and visible in the Aspire dashboard
-    .WithEnvironment("BROWSER", "none")
-
-    // Set a standard environment variable that your CRA setup can use
-    // to get the *resolved* URL of the backend API service.
-    // NOTE: Aspire automatically resolves the endpoint's URL at runtime.
+    .WithReference(apiService)     // Reference the API service if the frontend needs to call it
+    .WaitFor(apiService)
     .WithEnvironment("REACT_APP_API_URL", apiService.GetEndpoint("https"))
+    //.WithWorkingDirectory("../Cc.Ui")
+    //.WithHttpEndpoint(targetPort: 3000) // The port React actually uses
+    //.WithEnvironment("PORT", "3000") // Set port in environment
+    //.WithExplicitStart() // or "yarn", "dev" if using Vite
+    .WithEnvironment("BROWSER", "none")
+    .WithExternalHttpEndpoints()    // Makes the frontend URL externally available and visible in the Aspire dashboard
+                                   
+    .WithHttpEndpoint(env: "PORT");  // Set the port environment variable CRA expects (PORT is the convention)
 
-    // Set the port environment variable CRA expects (PORT is the convention)
-    .WithHttpEndpoint(env: "PORT");
+//.WithHttpHealthCheck("/health")
+// Set a standard environment variable that your CRA setup can use
+// to get the *resolved* URL of the backend API service.
+// NOTE: Aspire automatically resolves the endpoint's URL at runtime.
+
 
 builder.Build().Run();
